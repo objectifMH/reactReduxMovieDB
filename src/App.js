e776f41d73c1a38f59a79f082fc527b960d0d716
unknown <objectifmh@hotmail.com>
2020-05-17 22:26:25 +0200

    test import dans app.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Route, Switch, BrowserRouter as Router} from '../node_modules/react-router-dom';
 
 
--- src/Component/card.js
+++ src/Component/card.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './card.scss';
+//import './card.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faPlusCircle, faExpand, faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 
