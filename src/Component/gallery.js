42606abc8a7947d18cc9e3926551612cb30159ec
unknown <objectifmh@hotmail.com>
2020-05-18 23:42:12 +0200

    fix voir fiche

--- src/Component/gallery.js
+++ src/Component/gallery.js
@@ -4,7 +4,7 @@ import Card from './card.js';
 import axios from 'axios';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusSquare} from '@fortawesome/free-solid-svg-icons';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 export default class Gallery extends Component {
@@ -43,17 +43,19 @@ export default class Gallery extends Component {
     }
 
     componentDidMount() {
-        let random = Math.floor(Math.random(this.state.listFilm.length-1) * 10);
+        let random = Math.floor(Math.random(this.state.listFilm.length - 1) * 10);
 
-         let urlDiscover = 'https://api.themoviedb.org/3/discover/movie?api_key=369db2052a84d1a49d133d25a3983cbd&language=en-US&sort_by=popularity.desc';
+        let urlDiscover = 'https://api.themoviedb.org/3/discover/movie?api_key=369db2052a84d1a49d133d25a3983cbd&language=en-US&sort_by=popularity.desc';
 
         axios.get(urlDiscover)
             .then(res => {
                 const persons = res.data.results;
                 this.setState(
-                    prevState => ({ listFilm: persons,
-                                    filmJumbo: persons[random] }),
-                                    () => this.getSerie()
+                    prevState => ({
+                        listFilm: persons,
+                        filmJumbo: persons[random]
+                    }),
+                    () => this.getSerie()
                 );
             })
         //console.log(this.state.films);
@@ -72,17 +74,17 @@ export default class Gallery extends Component {
     getSerie(inputSearch) {
         console.log(inputSearch);
         let testSerie = 'https://api.themoviedb.org/3/discover/tv?api_key=369db2052a84d1a49d133d25a3983cbd&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false';
-        let suffixe = inputSearch !== undefined ? 'search/tv'+this.state.api_key+'&query='
-                                        : 'discover/tv?api_key=369db2052a84d1a49d133d25a3983cbd&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false';
+        let suffixe = inputSearch !== undefined ? 'search/tv' + this.state.api_key + '&query='
+            : 'discover/tv?api_key=369db2052a84d1a49d133d25a3983cbd&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false';
         console.log(inputSearch, this.state.urlBase.concat(suffixe, inputSearch));
-        axios.get(this.state.urlBase.concat(suffixe)+inputSearch)
+        axios.get(this.state.urlBase.concat(suffixe) + inputSearch)
             .then(res => {
                 const listSerie = res.data.results;
                 console.log(res.data.results)
                 this.setState(
                     prevState => ({ listSerie }),
                     () => console.log(listSerie)
-                                    
+
                 );
             })
     }
@@ -111,7 +113,7 @@ export default class Gallery extends Component {
 
     render() {
 
-        
+
         //console.log(this.state.listFilm[0]);
         return (
             <div className="comp-gallery m-3">
@@ -124,63 +126,65 @@ export default class Gallery extends Component {
                         <button className="btn" type="submit" onClick={this.clickSearch}>Rechercher</button>
                     </div>
                 </div>
-                { 
-                /** 
-                    this.state.filmJumbo != null ? 
-                    <div className="jumbotron" 
-                        style={{
-                        backgroundImage: "url(" + this.state.urlImage + this.state.filmJumbo['backdrop_path'] + ")"}}
-                    >
-                    <div>
-                            
-                        <h3><span className="box-shadow">{this.state.filmJumbo.title}</span></h3>
-                        <p>
-                        <span className="box-shadow">{this.state.filmJumbo['release_date']}</span>
-                        </p>
-                    </div>
-                    </div> :
-                    ''
-                */
+                {
+                    /** 
+                        this.state.filmJumbo != null ? 
+                        <div className="jumbotron" 
+                            style={{
+                            backgroundImage: "url(" + this.state.urlImage + this.state.filmJumbo['backdrop_path'] + ")"}}
+                        >
+                        <div>
+                                
+                            <h3><span className="box-shadow">{this.state.filmJumbo.title}</span></h3>
+                            <p>
+                            <span className="box-shadow">{this.state.filmJumbo['release_date']}</span>
+                            </p>
+                        </div>
+                        </div> :
+                        ''
+                    */
                 }
-                
+
 
                 <h3>Movies most Popular</h3>
                 {
-                    
+
                     <div>
-                    <div className="modal fade" id="myModal">
-                        <div className="modal-dialog modal-dialog-centered">
-                            <div className="modal-content">
+                        <div className="modal fade" id="myModal">
+                            <div className="modal-dialog modal-dialog-centered">
+                                <div className="modal-content">
 
-                                
-                                <div className="modal-header">
-                                    <h4 className="modal-title">{
-                                    this.state.filmCollapse.title ? 
-                                    this.state.filmCollapse.title : 
-                                    this.state.filmCollapse.name
-                                    }</h4>
-                                    <button type="button" className="close modal-close" data-dismiss="modal" title="fermer la modal">&times;</button>
+
+                                    <div className="modal-header">
+                                        <h4 className="modal-title">{
+                                            this.state.filmCollapse.title ?
+                                                this.state.filmCollapse.title :
+                                                this.state.filmCollapse.name
+                                        }</h4>
+                                        <button type="button" className="close modal-close" data-dismiss="modal" title="fermer la modal">&times;</button>
+                                    </div>
+                                    <div className="modal-body" >
+                                        {this.state.filmCollapse['backdrop_path'] === null ? <img className="card-img-top" src={process.env.PUBLIC_URL + '/no_image.png'} alt={this.state.filmCollapse.title} /> : <img className="card-img-top" src={this.state.urlImage + this.state.filmCollapse['backdrop_path']} alt={this.state.filmCollapse.title} />}
+
+                                        <hr></hr>
+                                        <p className="card-text">{this.state.filmCollapse.overview}</p>
+                                        <p className="card-text">{this.state.filmCollapse.release_date}
+                                            <Link className="nav-link close" to={
+
+                                                this.state.filmCollapse.title ?
+                                                    '/film/' + this.state.filmCollapse.id + '/movie' :
+                                                    '/film/' + this.state.filmCollapse.id + '/tv'
+                                            } >
+                                                <span  className="btn btn-success btn-sm mt-2 ml-3" data-toggle="modal" data-target="#myModal">Voir fiche</span>
+                                            </Link>
+                                        </p>
+                                    </div>
+
+
+
                                 </div>
-                                <div className="modal-body" >
-                                    {this.state.filmCollapse['backdrop_path']=== null ? <img className="card-img-top"  src={process.env.PUBLIC_URL +'/no_image.png'}  alt={this.state.filmCollapse.title}/>: <img className="card-img-top" src={this.state.urlImage + this.state.filmCollapse['backdrop_path']} alt={this.state.filmCollapse.title} />}
-                    
-                                    <hr></hr>
-                                    <p className="card-text">{this.state.filmCollapse.overview}</p>
-                                    <p className="card-text">{this.state.filmCollapse.release_date}</p>
-                                </div>
-
-                            <Link className="nav-link close" to={
-                                
-                                this.state.filmCollapse.title ? 
-                                '/film/'+this.state.filmCollapse.id+'/movie' : 
-                                '/film/'+this.state.filmCollapse.id+'/tv'
-                                } >
-                                <FontAwesomeIcon icon={faPlusSquare} data-toggle="modal" data-target="#myModal" alt={'En savoir plus sur '+this.state.filmCollapse.title}/>
-                            </Link>
-
                             </div>
                         </div>
-                    </div>
 
                     </div>
 
@@ -188,13 +192,13 @@ export default class Gallery extends Component {
                 <div className="container-fluid color-grey div-cont ">
                     {
                         this.state.listFilm.map((film, index) => (
-                                index <=  (this.state.inputSearch === '' ? 13 : this.state.listFilm.length)  ? 
+                            index <= (this.state.inputSearch === '' ? 13 : this.state.listFilm.length) ?
                                 <div key={index}>
                                     <Card key={index} film={film} tv={false} clickCollapse={this.showCollapse}></Card>
 
 
                                 </div> : ""
-                            )
+                        )
                         )
                     }
 
@@ -208,13 +212,13 @@ export default class Gallery extends Component {
                 <div className="container-fluid color-grey div-cont ">
                     {
                         this.state.listSerie.map((serie, index) => (
-                            index <=  (this.state.inputSearch === '' ? 13 : this.state.listSerie.length)  ? 
+                            index <= (this.state.inputSearch === '' ? 13 : this.state.listSerie.length) ?
                                 <div key={index}>
                                     <Card key={index} film={serie} tv={true} clickCollapse={this.showCollapse}></Card>
 
 
                                 </div> : ""
-                            )
+                        )
                         )
                     }
 
