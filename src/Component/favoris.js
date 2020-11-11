30e7850587ffa8bffa5978e607895a24ad4097ce
unknown <objectifmh@hotmail.com>
2020-11-11 02:45:56 +0100

    final

--- src/Component/favoris.js
+++ src/Component/favoris.js
@@ -4,9 +4,6 @@ import { connect } from 'react-redux';
 //import './favoris.scss';
 
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faTrash } from '@fortawesome/free-solid-svg-icons';
-
 class Favoris extends Component {
     
     supprimeFilmFav = (film) => {
